555b4929801fcab2936b15329498bcd927858ad9
elimali hamza <elimal.hamza@gmail.com>
2024-02-03 06:57:41 -0800

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,12 +14,15 @@ def close_storage_connection(exception):
     """Close the storage connection."""
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found_error(err):
     response_error = {"error": "Not Found"}
+    """returns a JSON-formatted 404 status code response."""
     return make_response(jsonify(response_error), 404)
+
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, debug=True, threaded=True)
