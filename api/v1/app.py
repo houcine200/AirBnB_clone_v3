ae1fc65d4f7e53e233793e5be16114cd333f3cb9
elimali hamza <elimal.hamza@gmail.com>
2024-02-03 06:47:39 -0800

    error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module defining the Flask application and API configuration."""
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,10 @@ def close_storage_connection(exception):
     """Close the storage connection."""
     storage.close()
 
+@app.errorhandler(404)
+def not_found_error(err):
+    response_error = {"error": "Not Found"}
+    return make_response(jsonify(response_error), 404)
 
 if __name__ == '__main__':
     host = getenv('HBNB_API_HOST', '0.0.0.0')
