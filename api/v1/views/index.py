a109baee938646839c93716bfd28f9663a5affa0
Houcine Walaq <houcine.walaq@gmai.com>
2024-02-04 20:00:18 +0100

    task 4 add method get

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
     """Retrieves the number of each object type"""
     stats = {
