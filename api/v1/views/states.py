708765868cf454e6825ea130105d2e1fb9585950
elimali hamza <elimal.hamza@gmail.com>
2024-02-04 03:21:05 -0800

    pycode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,6 +5,7 @@ from flask import jsonify, abort, make_response, request
 from models import storage
 from models.state import State
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """Get all State objects."""
@@ -14,6 +15,7 @@ def get_all_states():
         list_states.append(state.to_dict())
     return jsonify(list_states)
 
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """Get a State object by id"""
@@ -22,7 +24,9 @@ def get_state(state_id):
         abort(404)
     return jsonify(state.to_dict())
 
-@app_views.route('/states/<string:state_id>', methods=['DELETE'], strict_slashes=False)
+
+@app_views.route('/states/<string:state_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Delete a State object by id."""
     state = storage.get(State, state_id)
@@ -31,6 +35,7 @@ def delete_state(state_id):
     state.delete()
     storage.save()
     return make_response(jsonify({}), 200)
+
 
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def create_state():
@@ -43,6 +48,7 @@ def create_state():
     new_state = State(**data)
     new_state.save()
     return jsonify(new_state.to_dict()), 201
+
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def update_state(state_id):
